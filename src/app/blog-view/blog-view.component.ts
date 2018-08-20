bff6df9db8d85eb20675b49670cb34dc7cc2219a
Your Name <you@example.com>
2018-08-20 18:30:27 +0530

    20 aug commit

--- src/app/blog-view/blog-view.component.ts
+++ src/app/blog-view/blog-view.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+//import route related code
+import {ActivatedRoute,Router} from "@angular/router";
+import { BlogService } from '../blog.service';
 @Component({
   selector: 'app-blog-view',
   templateUrl: './blog-view.component.html',
-  styleUrls: ['./blog-view.component.css']
+  styleUrls: ['./blog-view.component.css'],
+  
 })
-export class BlogViewComponent implements OnInit {
+export class BlogViewComponent implements OnInit,OnDestroy{
 
-  constructor() { }
+  public currentBlog;
+  
+
+  constructor(private _route:ActivatedRoute,private router:Router,public blogService:BlogService) {
+console.log("constructor is called");
+   }
+
+   ngOnDestroy():void{
+    console.log("Destroy from blog view is called");
+   }
 
   ngOnInit() {
+    console.log("nginit from blog view is called");
+
+    //get blog id from
+    let myBlogId = this._route.snapshot.paramMap.get('blogId');
+    console.log(myBlogId);
+   this.currentBlog = this.blogService.getSingleInformation(myBlogId);
+   console.log(this.currentBlog);
   }
 
+ 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BlogService } from '../blog.service';
 
 
 //decorator
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  
 })
 
 // a simple class
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit,OnDestroy{
 
-  constructor() { }
+  public allBlogs;
+
+  constructor(private blogService:BlogService) {
+    console.log("Home component constuctor is called");
+   }
 
   ngOnInit() {
+console.log("Home component init is called");
+ this.allBlogs = this.blogService.getAllBlogs();
+ console.log(this.allBlogs);
   }
 
+  ngOnDestroy() {
+    console.log("Home componentt Destroy is called")
+  }
 }
